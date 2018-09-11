1e5faea77da171a43f51cde75e88fa15c9d406d6
Eddie Kohler <ekohler@gmail.com>
2018-09-11 12:43:06 -0400

    Prepare again.

--- datarep3/mexplore.cc
+++ datarep3/mexplore.cc
@@ -2,25 +2,21 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <string.h>
+#include <stddef.h>
 #include "hexdump.hh"
 
-int global_i = 65000000;
-const int const_global_i = 66;
-
 void f() {
-    int local_i = 1;
-    int local_n = -1;
-    long long local_ll = 83421972397451239;
-    int* allocated_i = new int(68);
+    int i1 = 0, i2 = 1, i3 = 2;
+    char c1 = 3;
+    char c2 = 4;
+    char c3 = 5;
 
-    printf("%d\n", global_i);
-    hexdump(&global_i, sizeof(int));
-    hexdump(&const_global_i, sizeof(int));
-    hexdump(&local_i, sizeof(local_i));
-    hexdump(&local_n, sizeof(local_n));
-    hexdump(&local_ll, sizeof(local_ll));
-    hexdump(allocated_i, sizeof(int));
-    hexdump(&allocated_i, sizeof(allocated_i));
+    hexdump(&i1, sizeof(i1));
+    hexdump(&i2, sizeof(i2));
+    hexdump(&i3, sizeof(i3));
+    hexdump(&c1, sizeof(c1));
+    hexdump(&c2, sizeof(c2));
+    hexdump(&c3, sizeof(c3));
 }
 
 int main() {
